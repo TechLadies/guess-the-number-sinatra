7b62d74d82771eaa7e4d8e3c7547365a032e48bc
huijing <kakyou_tensai@yahoo.com>
2016-08-12 00:30:56 +0800

    Add nitrous specific config to app.rb

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require "sinatra/reloader" if development?
 
 enable :sessions
 
+configure :development do
+  set :bind, '0.0.0.0'
+  set :port, 3000
+end
+
 helpers do
   def guessed_number
     session['guessed_number']
